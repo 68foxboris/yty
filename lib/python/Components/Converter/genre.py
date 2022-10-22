4ff311ea96aaaf23c7f992107e2985be297439c9
68foxboris <boris.fox.solovyev@gmail.com>
2022-10-22 20:14:00 +0300

    Update genre.py

--- lib/python/Components/Converter/genre.py
+++ lib/python/Components/Converter/genre.py
@@ -4,22 +4,28 @@
 # some broadcaster do define other types so this list
 # may grow or be replaced..
 #
+
+
 class GenresETSI:
 	maintype = (
 		_("Reserved"),
-		_("Drama and Films"),
-		_("News/Current Affairs/Social"),
-		_("Show/Game Show/Leisure hobbies"),
+		_("Movie/Drama"),
+		_("News/Current Affairs"),
+		_("Show/Games show"),
 		_("Sports"),
-		_("Children/Youth/Education/Science"),
+		_("Children/Youth"),
 		_("Music/Ballet/Dance"),
-		_("Arts/Culture (without music)")
-		)
+		_("Arts/Culture"),
+		_("Social/Political/Economics"),
+		_("Education/Science/Factual"),
+		_("Leisure hobbies"),
+		_("Other")
+	)
 
 	subtype = {
 		# Movie/Drama
 		1: (
-			_("movie (general)"),
+			_("movie/drama (general)"),
 			_("detective/thriller"),
 			_("adventure/western/war"),
 			_("science fiction/fantasy/horror"),
@@ -27,7 +33,7 @@ class GenresETSI:
 			_("soap/melodrama/folkloric"),
 			_("romance"),
 			_("serious/classical/religious/historical movie/drama"),
-			_("adult movie")
+			_("adult movie/drama")
 		),
 		# News/Current Affairs
 		2: (
@@ -35,25 +41,14 @@ class GenresETSI:
 			_("news/weather report"),
 			_("news magazine"),
 			_("documentary"),
-			_("discussion/interview/debate"),
-			_("social/political issues/economics (general)"),
-			_("magazines/reports/documentary"),
-			_("economics/social advisory"),
-			_("remarkable people")
+			_("discussion/interview/debate")
 		),
 		# Show/Game show
 		3: (
 			_("show/game show (general)"),
 			_("game show/quiz/contest"),
 			_("variety show"),
-			_("talk show"),
-			_("leisure hobbies (general)"),
-			_("tourism/travel"),
-			_("handicraft"),
-			_("motoring"),
-			_("fitness and health"),
-			_("cooking"),
-			_("advertisement/shopping")
+			_("talk show")
 		),
 		# Sports
 		4: (
@@ -62,31 +57,22 @@ class GenresETSI:
 			_("sports magazine"),
 			_("football/soccer"),
 			_("tennis/squash"),
-			_("team sports/excluding football"),
+			_("team sports"),
 			_("athletics"),
 			_("motor sport"),
 			_("water sport"),
 			_("winter sport"),
 			_("equestrian"),
-			_("martial sports"),
-			_("local sports")
+			_("martial sports")
 		),
 		# Children/Youth
 		5: (
-			_("children's youth program (general)"),
+			_("children's/youth program (general)"),
 			_("pre-school children's program"),
 			_("entertainment (6-14 year old)"),
 			_("entertainment (10-16 year old)"),
 			_("information/education/school program"),
-			_("cartoon/puppets"),
-			_("educational/science/factual topics (general)"),
-			_("nature/animals/environment"),
-			_("technology/natural sciences"),
-			_("medicine/physiology/psychology"),
-			_("foreign countries/expeditions"),
-			_("social/spiritual sciences"),
-			_("further education"),
-			_("languages")
+			_("cartoon/puppets")
 		),
 		# Music/Ballet/Dance
 		6: (
@@ -98,7 +84,7 @@ class GenresETSI:
 			_("musical/opera"),
 			_("ballet")
 		),
-		# Arts/Culture (without music)
+		# Arts/Culture
 		7: (
 			_("arts/culture (without music, general)"),
 			_("performing arts"),
@@ -112,6 +98,43 @@ class GenresETSI:
 			_("new media"),
 			_("arts/culture magazine"),
 			_("fashion")
+		),
+		# Social/Political/Economics
+		8: (
+			_("social/political issues/economics (general)"),
+			_("magazines/reports/documentary"),
+			_("economics/social advisory"),
+			_("remarkable people")
+		),
+		# Education/Science/...
+		9: (
+			_("education/science/factual topics (general)"),
+			_("nature/animals/environment"),
+			_("technology/natural science"),
+			_("medicine/physiology/psychology"),
+			_("foreign countries/expeditions"),
+			_("social/spiritual science"),
+			_("further education"),
+			_("languages")
+		),
+
+		# Leisure hobies
+		10: (
+			_("leisure hobbies (general)"),
+			_("tourism/travel"),
+			_("handicraft"),
+			_("motoring"),
+			_("fitness & health"),
+			_("cooking"),
+			_("advertisement/shopping"),
+			_("gardening")
+		),
+		# Other
+		11: (
+			_("original language"),
+			_("black & white"),
+			_("unpublished"),
+			_("live broadcast")
 		),
 	}
 
@@ -238,9 +261,11 @@ def getGenreStringMain(hn, ln, country=None):
 	countryInfo = countries.get(country, defaultCountryInfo)
 	return countryInfo[0](hn, ln, countryInfo[2])
 
+
 def getGenreStringSub(hn, ln, country=None):
 	countryInfo = countries.get(country, defaultCountryInfo)
 	return countryInfo[1](hn, ln, countryInfo[2])
+
 
 def getGenreStringLong(hn, ln, country=None):
 	# if hn == 0:
